9f8b4ca85efa7d9abcdf43765f311ca9fdc529b0
Clément Hubert <clement@plopoyop.me>
2025-04-24 10:25:14 +0200

    add devenv

---
+++ devenv.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+  env.LANG="en_US.UTF8";
+
+  packages = [
+    pkgs.ansible
+    pkgs.ansible-doctor
+    pkgs.git
+    pkgs.python312Packages.kubernetes
+    pkgs.python312Packages.molecule
+    pkgs.python312Packages.molecule-plugins
+    pkgs.yamllint
+ ];
+
+  languages.ansible.enable = true;
+
+  enterShell = ''
+    export ANSIBLE_ROLES_PATH=$PWD/.devenv/ansible/roles;
+    export ANSIBLE_COLLECTIONS_PATH=$PWD/.devenv/ansible/collections;
+
+    mkdir -p $ANSIBLE_ROLES_PATH
+    mkdir -p $ANSIBLE_COLLECTIONS_PATH
+  '';
+}
